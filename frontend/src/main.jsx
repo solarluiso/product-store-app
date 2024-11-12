844f5a824bb70017d30e2de48403b252fa6d06e2
Luis <solarluiso@gmail.com>
2024-11-12 08:46:07 -0500

    frontend setup - folder structure, install dependencies

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "./components/ui/provider.jsx";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
