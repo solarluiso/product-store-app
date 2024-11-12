8e7a451ae32100a1adc60e10c34555df5539ede1
Luis <solarluiso@gmail.com>
2024-11-11 20:21:57 -0500

    backend development - file structure, database connection

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
