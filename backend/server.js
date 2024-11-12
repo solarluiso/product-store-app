4053a250043bd216ec7503a2a71a4881512507ee
Luis <solarluiso@gmail.com>
2024-11-11 20:23:26 -0500

    CRUD implementation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
